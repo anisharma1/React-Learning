7a36e920c654c0bedc676994f56268ca1a35c415
anisharma1 <111197294+anisharma1@users.noreply.github.com>
2023-09-03 22:48:09 +0530

    Intro to JSX

---
+++ 2. jsx-code-challenge-forked/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>Flowers I love!</h1>
+    <ul>
+      <li>Roses</li>
+      <li>Lilly</li>
+      <li>Jasmin</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);

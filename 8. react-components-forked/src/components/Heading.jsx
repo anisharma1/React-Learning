c617df50f94052f17edcd7944760e365b984ca79
anisharma1 <111197294+anisharma1@users.noreply.github.com>
2023-09-04 14:59:13 +0530

    React Components

---
+++ 8. react-components-forked/src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Heading() {
+  //use PascalCase for function names
+  //first letter is capatalized
+  return <h1>My Favourite Foods</h1>;
+}
+
+export default Heading;

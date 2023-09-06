5ce3cf40657cd22ad9cd2b3d54f581aafffac4dc
anisharma1 <111197294+anisharma1@users.noreply.github.com>
2023-09-06 17:40:39 +0530

    ES6 Destructuring

---
+++ 23. es6-destructuring-forked/src/index.js
@@ -0,0 +1,35 @@
+import animals, { useAnimals } from "./data";
+// console.log(animals);
+
+const [cat1, dog, cat2] = animals;
+
+console.log(useAnimals(cat1));
+
+const [name, makeSound] = useAnimals(cat1);
+
+console.log(name);
+makeSound();
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
---
+++ 24. es6-destructuring-challenge-forked/src/data.js
@@ -0,0 +1,24 @@
+const animals = [
+  {
+    name: "cat",
+    sound: "meow",
+    feed: {
+      food: 2,
+      water: 3
+    }
+  },
+  { name: "dog", sound: "woof" },
+  { sound: "meow" }
+];
+
+function useAnimals(animal) {
+  return [
+    animal.name,
+    function () {
+      console.log(animal.sound);
+    }
+  ];
+}
+
+export default animals;
+export { useAnimals };
